d0dc8fce8a3ad0e399690a226acc6cfc28741498
alett1 <lett.alexandre@gmail.com>
2014-12-05 01:19:15 +0100

    mission

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.home',
-  'myApp.view2',
+  'myApp.mission',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/volunteer/mission/mission.js
+++ app/volunteer/mission/mission.js
@@ -19,15 +19,16 @@ describe('myApp.mission module', function() {
 angular.module('myApp.mission', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/mission/:id', {
+      $routeProvider.when('/missions/:id', {
         templateUrl: '/volunteer/mission/mission.html',
         controller: 'View2Ctrl'
       });
     }])
 
-.controller('View2Ctrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+.controller('View2Ctrl', ['$scope', '$routeParams', function($scope, $routeParams) {
   var loremipsum = "Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium, cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia mea. Rei publicae satis erat tot dierum quot C. Mario ; dis immortalibus non erat exigua eadem gratulatio quae ex maximis bellis. Ergo ille cumulus dierum hominis est dignitati tributus.";
 
-  $scope.mission =
-    {"id": stateParams.id, "name": "toto", "description": loremipsum}
+  $scope.mission = {"id": $routeParams.id, "title": "Mission restos du coeur", "description": loremipsum, "nbVolunteers": 5, "maxVolunteers": 10};
+  $scope.user = {"id": "1", "lastName": "Walsh", "firstName": "Paul", "experience": "300"};
+
 }]);
